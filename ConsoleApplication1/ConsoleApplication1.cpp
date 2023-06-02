ae8e1b70e5b2a2dfe76db27cf27dd0ca9d4fd2d0
Audrius Domarkas <Audrius.Domarkas@mif.stud.vu.lt>
2023-06-02 12:57:16 +0300

    klase1.2

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,85 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include "Header.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<studentas> stud;
+    vector<studentas> geri;
+    vector<studentas> blogi;
+    srand(time(NULL));
+    studentas dab_stud;
+
+    string ivestis;
+    bool tinkama_ivestis = false;
+    string pasirinkimas;
+
+    while (!tinkama_ivestis)
+    {
+        cout << "Pasirinkite pazymiu ivedimo metoda ('R' - ranka, 'F' - failas, 'G' - generavimas): ";
+        cin >> ivestis;
+
+        if (ivestis == "R" || ivestis == "r")
+        {
+            do
+            {
+                studento_nuskaitymas(dab_stud);
+                stud.push_back(dab_stud);
+
+                cout << "Studentu vedimo nutraukimas 'N', Tesimas 'Betkoks simbolis': ";
+                cin >> ivestis;
+
+            } while (ivestis != "n" && ivestis != "N");
+            studento_spausdinimas(stud);
+            tinkama_ivestis = true;
+        }
+        if (ivestis == "G" || ivestis == "g")
+        {
+            cout << "Kas jums svarbiau: funkcionalumas F, programos talpa T: "; cin >> pasirinkimas;
+            if (pasirinkimas == "F" || pasirinkimas == "f")
+            {
+                int studentu_skaicius;
+                int pazymiu_skaicius;
+                string generuoto_failo_pavadinimas;
+
+                cout << "Iveskite pavadinima, kuriame bus sugeneruoti duomenys: "; cin >> generuoto_failo_pavadinimas;
+                cout << "Iveskite studentu skaiciu, kuris bus sugeneruotas: "; cin >> studentu_skaicius;
+                cout << "Iveskite pazmymiu skaiciu, kuris bus sugeneruotas: "; cin >> pazymiu_skaicius;
+
+                generuoti_faila_duomenis(studentu_skaicius, pazymiu_skaicius, generuoto_failo_pavadinimas);
+                stud = failo_nuskaitymas2(generuoto_failo_pavadinimas);
+                tinkama_ivestis = true;
+            }
+            else if (pasirinkimas == "T" || pasirinkimas == "t")
+            {
+                int studentu_skaicius;
+                int pazymiu_skaicius;
+                string generuoto_failo_pavadinimas = "demonai";
+
+                cout << "Iveskite studentu skaiciu, kuris bus sugeneruotas: "; cin >> studentu_skaicius;
+                cout << "Iveskite pazmymiu skaiciu, kuris bus sugeneruotas: "; cin >> pazymiu_skaicius;
+                generuoti_faila_duomenis(studentu_skaicius, pazymiu_skaicius, generuoto_failo_pavadinimas);
+                stud = failo_nuskaitymas3(generuoto_failo_pavadinimas);
+                tinkama_ivestis = true;
+            }
+        }
+        else if (ivestis == "F" || ivestis == "f")
+        {
+            string file_name;
+            cout << "Iveskite failo pavadinima: ";
+            cin >> file_name;
+
+            stud = failo_nuskaitymas1(file_name);
+            studento_spausdinimas(stud);
+
+            if (!stud.empty())
+                tinkama_ivestis = true;
+            else
+                cout << "Failas neegzistuoja arba yra tuscias. Bandykite dar karta." << endl;
+        }
+        else
+        {
+            cout << "Neteisingas pasirinkimas. Bandykite dar karta." << endl;
+        }
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
